86114e18c558e335742c6be953b7f4e75d54417e
unknown <heehyeonljeong@gmail.com>
2022-05-15 17:08:17 -0700

    removed deprecated modules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import OAuth from "oauth-1.0a"
 import crypto from "crypto"
-import request from "request"
+import axios from "axios"
 
 let oauth
 let pooauth
@@ -21,89 +21,71 @@ class SchoologyAPI {
     })
   }
 
-  request(path, body = null, method = null ? "POST" : "GET") {
+  request(path, body = null, method = "GET") {
     const url = "https://api.schoology.com/v1" + path
     return new Promise((resolve, reject) => {
-      request({
-        url,
-        method,
+      axios.get(url, {
+        responseType: "json",
         body: body && JSON.stringify(body),
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-          ...oauth.toHeader(oauth.authorize({ url, method }))
-        }
-      }, (err, { statusCode }, body) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(JSON.parse(body))
-        }
+        headers: { ...oauth.toHeader(oauth.authorize({ url, method })) },
       })
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          const requestJSON = JSON.parse(JSON.stringify(error))
+          console.error(requestJSON.code)
+        })
     })
   }
 
-  createRequestToken(body = null, method = null ? "POST" : "GET") {
+  createRequestToken(body = null, method = "GET") {
     const url = "https://api.schoology.com/v1/oauth/request_token"
     return new Promise((resolve, reject) => {
-      request({
-        url,
-        method,
+      axios.get(url, {
+        responseType: "json",
         body: body && JSON.stringify(body),
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-          ...oauth.toHeader(oauth.authorize({ url, method }))
-        }
-      }, (err, { statusCode }, body) => {
-        if (err) {
-          reject(err)
-        }
-        else {
-          resolve(body)
-        }
+        headers: { ...oauth.toHeader(oauth.authorize({ url, method })) },
       })
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          const requestJSON = JSON.parse(JSON.stringify(error))
+          console.error(requestJSON.code)
+        })
     })
   }
 
-  getAccessToken(requestToken, body = null, method = "POST") {
-    const url = "https://api.schoology.com/v1/oauth/access_token"
-    const oauthKey = requestToken.slice(12, 53)
-    const oauthSecret = requestToken.slice(73, 105)
-    const token = {
-      key: oauthKey,
-      secret: oauthSecret,
-    }
-    pooauth = OAuth({
-      consumer: { key, secret },
-      signature_method: "HMAC-SHA1",
-      hash_function(base_string, key) {
-        return crypto
-          .createHmac("sha1", key)
-          .update(base_string)
-          .digest("base64")
-      }
-    })
-    return new Promise((resolve, reject) => {
-      request({
-        url,
-        method,
-        body: body && JSON.stringify(body),
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-          ...pooauth.toHeader(pooauth.authorize({ url, method }))
-        }
-      }, (err, { statusCode }, body) => {
-        if (err) {
-          reject(err)
-        }
-        else {
-          resolve(body)
-        }
-      })
-    })
-  }
+  //getAccessToken(requestToken, body = null, method = "GET") {
+  //  const url = "https://api.schoology.com/v1/oauth/access_token"
+  //  const parsedRequestToken = this.parseRequestToken(requestToken)
+  //  pooauth = OAuth({
+  //    consumer: { key, secret },
+  //    signature_method: "HMAC-SHA1",
+  //    hash_function(base_string, key) {
+  //      return crypto
+  //        .createHmac("sha1", key)
+  //        .update(base_string)
+  //        .digest("base64")
+  //    }
+  //  })
+  //  return new Promise((resolve, reject) => {
+  //    axios.get(url, {
+  //      responseType: "json",
+  //      body: body && JSON.stringify(body),
+  //      headers: { ...pooauth.toHeader(pooauth.authorize({ url, method })) },
+  //    })
+  //      .then(response => {
+  //        resolve(response.data)
+  //      })
+  //      .catch(error => {
+  //        const requestJSON = JSON.parse(JSON.stringify(error))
+  //        console.error(requestJSON.code)
+  //      })
+  //  })
+  //}
+
   parseRequestToken(input) {
     const finalKey = input.slice(12, 53)
     const finalSecret = input.slice(73, 105)
