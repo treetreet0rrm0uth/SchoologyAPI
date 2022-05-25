0c076825fe75f5b352de6b91c3fc913c144ba93f
unknown <heehyeonljeong@gmail.com>
2022-05-24 22:58:04 -0700

    commit

--- index.js
+++ index.js
@@ -1,98 +1,94 @@
-import crypto from "crypto"
-import axios from "axios"
-import OAuth from "oauth-1.0a"
-
-let oauth
-let pooauth
-let key
-let secret
+const axios = require("axios")
+const request = require("request")
 
 class SchoologyAPI {
   constructor(key, secret) {
-    oauth = OAuth({
-      consumer: { key, secret },
-      signature_method: "HMAC-SHA1",
-      hash_function(base_string, key) {
-        return crypto
-          .createHmac("sha1", key)
-          .update(base_string)
-          .digest("base64")
-      }
-    })
+    this.key = key
+    this.secret = secret
   }
 
-  request(path, body = null, method = "GET") {
+  request(path, tokenKey, tokenSecret) {
+    const key = this.key
+    const secret = this.secret
     const url = "https://api.schoology.com/v1" + path
     return new Promise((resolve, reject) => {
-      axios.get(url, {
-        responseType: "json",
-        body: body && JSON.stringify(body),
-        headers: { ...oauth.toHeader(oauth.authorize({ url, method })) },
+      request.get(url, {
+        oauth: {
+          consumer_key: key,
+          consumer_secret: secret,
+          token: tokenKey,
+          token_secret: tokenSecret
+        }
+      }, function (err, res, body) {
+        resolve(JSON.parse(body))
       })
-        .then(response => {
-          resolve(response.data)
-        })
-        .catch(error => {
-          const requestJSON = JSON.parse(JSON.stringify(error))
-          console.error(requestJSON.code)
-        })
     })
   }
 
   createRequestToken(body = null, method = "GET") {
+    const key = this.key
+    const secret = this.secret
     const url = "https://api.schoology.com/v1/oauth/request_token"
     return new Promise((resolve, reject) => {
-      axios.get(url, {
-        responseType: "json",
-        body: body && JSON.stringify(body),
-        headers: { ...oauth.toHeader(oauth.authorize({ url, method })) },
+      request.get(url, {
+        oauth: {
+          consumer_key: key,
+          consumer_secret: secret,
+        }
+      }, function (err, res, body) {
+        resolve(body)
       })
-        .then(response => {
-          resolve(response.data)
-        })
-        .catch(error => {
-          const requestJSON = JSON.parse(JSON.stringify(error))
-          console.error(requestJSON.code)
-        })
     })
   }
 
-  //getAccessToken(requestToken, body = null, method = "GET") {
-  //  const url = "https://api.schoology.com/v1/oauth/access_token"
-  //  const parsedRequestToken = this.parseRequestToken(requestToken)
-  //  pooauth = OAuth({
-  //    consumer: { key, secret },
-  //    signature_method: "HMAC-SHA1",
-  //    hash_function(base_string, key) {
-  //      return crypto
-  //        .createHmac("sha1", key)
-  //        .update(base_string)
-  //        .digest("base64")
-  //    }
-  //  })
-  //  return new Promise((resolve, reject) => {
-  //    axios.get(url, {
-  //      responseType: "json",
-  //      body: body && JSON.stringify(body),
-  //      headers: { ...pooauth.toHeader(pooauth.authorize({ url, method })) },
-  //    })
-  //      .then(response => {
-  //        resolve(response.data)
-  //      })
-  //      .catch(error => {
-  //        const requestJSON = JSON.parse(JSON.stringify(error))
-  //        console.error(requestJSON.code)
-  //      })
-  //  })
-  //}
+  getAccessToken(requestToken) {
+    const key = this.key
+    const secret = this.secret
+    const url = "https://api.schoology.com/v1/oauth/access_token"
+    const parsedRequestToken = this.parseRequestToken(requestToken)
+    const token = {
+      key: parsedRequestToken.finalKey,
+      secret: parsedRequestToken.finalSecret
+    }
+    return new Promise((resolve, reject) => {
+      request.get(url, {
+        oauth: {
+          consumer_key: key,
+          consumer_secret: secret,
+          token: token.key,
+          token_secret: token.secret
+        }
+      }, function (err, res, body) {
+        resolve(body)
+      })
+    })
+  }
 
   parseRequestToken(input) {
     const finalKey = input.slice(12, 53)
     const finalSecret = input.slice(73, 105)
     return { finalKey, finalSecret }
   }
+
+  clientRequest(path, finalToken, finalSecret) {
+    const key = this.key
+    const secret = this.secret
+    const url = "https://api.schoology.com/v1" + path
+    return new Promise((resolve, reject) => {
+      request.get(url, {
+        oauth: {
+          consumer_key: key,
+          consumer_secret: secret,
+          token: finalToken,
+          token_secret: finalSecret
+        }
+      }, function (err, res, body) {
+        resolve(JSON.parse(body))
+      })
+    })
+  }
 }
 
-export default SchoologyAPI
+module.exports = SchoologyAPI
 
 //tree tree t0rr m0uth
